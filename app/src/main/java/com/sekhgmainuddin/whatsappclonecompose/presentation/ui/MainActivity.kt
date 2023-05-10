ab718857d3f765c384a757443f47cea336dc88b5
Sekh Gulam Mainuddin <sekhgmainuddin@gmail.com>
2023-05-11 01:03:04 +0530

    Welcome Screen UI Added

--- app/src/main/java/com/sekhgmainuddin/whatsappclonecompose/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/sekhgmainuddin/whatsappclonecompose/presentation/ui/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.sekhgmainuddin.whatsappclonecompose
+package com.sekhgmainuddin.whatsappclonecompose.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,18 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = whatsAppBackground
                 ) {
-                    Greeting(name = "Hello WhatsApp")
                 }
             }
         }
     }
 }
 
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
--- app/src/main/java/com/sekhgmainuddin/whatsappclonecompose/presentation/ui/WelcomeActivity.kt
+++ app/src/main/java/com/sekhgmainuddin/whatsappclonecompose/presentation/ui/WelcomeActivity.kt
@@ -1,10 +1,12 @@
 package com.sekhgmainuddin.whatsappclonecompose.presentation.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,6 +21,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -28,13 +33,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -42,16 +43,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
@@ -65,6 +69,11 @@ import com.sekhgmainuddin.whatsappclonecompose.ui.theme.secondaryTextColor
 import com.sekhgmainuddin.whatsappclonecompose.ui.theme.whatsAppBackground
 import java.util.Locale
 
+enum class WelcomeScreen {
+    welcomeScreen,
+    phoneNumberVerfication,
+    updateProfileInfo
+}
 
 class WelcomeActivity : ComponentActivity() {
 
@@ -74,20 +83,32 @@ class WelcomeActivity : ComponentActivity() {
         val phoneCodeAndCountryList = getCountryAndCodes()
         val countryList = phoneCodeAndCountryList.values.toList()
         val phoneCodes = phoneCodeAndCountryList.keys.toList()
+
         setContent {
 
-            var agreeAndContinue by remember {
-                mutableStateOf(true)
+            var welcomeState by remember {
+                mutableStateOf(WelcomeScreen.welcomeScreen)
             }
 
-            if (agreeAndContinue) {
-                PhoneNumberVerifyCompose(countryList)
-            } else {
-                WelcomeScreenCompose(
-                    onAgreeButtonClicked = {
-                        agreeAndContinue = true
-                    },
-                )
+            when (welcomeState) {
+                WelcomeScreen.welcomeScreen -> {
+                    WelcomeScreenCompose(
+                        onAgreeButtonClicked = {
+                            welcomeState = WelcomeScreen.phoneNumberVerfication
+                        },
+                    )
+                }
+                WelcomeScreen.phoneNumberVerfication -> {
+                    PhoneNumberVerifyCompose(countryList, phoneCodes) {
+                        welcomeState = WelcomeScreen.updateProfileInfo
+                    }
+                }
+                else -> {
+                    UpdateProfileInfoCompose{
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }
+                }
             }
         }
 
@@ -195,17 +216,22 @@ fun WelcomeScreenCompose(
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PhoneNumberVerifyCompose(countryList: List<String>) {
+fun PhoneNumberVerifyCompose(
+    countryList: List<String>,
+    countryCodeList: List<Int>,
+    onVerificationComplete: () -> Unit
+) {
 
-    var countryCode by remember {
-        mutableStateOf(1)
-    }
-    var mExpanded by remember { mutableStateOf(false) }
-    var mSelectedText by remember { mutableStateOf("United States") }
+    var phoneNumber by remember { mutableStateOf("") }
+    var countrySelection by remember { mutableStateOf(false) }
+    var countryCodeSelection by remember { mutableStateOf(false) }
+    var selectedCountry by remember { mutableStateOf("United States") }
+    var selectedCountryCode by remember { mutableStateOf(1) }
+    var isOTPSent by remember { mutableStateOf(false) }
+    var OTP by remember { mutableStateOf("") }
 
-    val icon = if (mExpanded)
+    val icon = if (countrySelection)
         Icons.Filled.KeyboardArrowUp
     else
         Icons.Filled.ArrowDropDown
@@ -215,6 +241,8 @@ fun PhoneNumberVerifyCompose(countryList: List<String>) {
             .fillMaxSize()
             .background(color = whatsAppBackground)
             .padding(20.dp)
+            .padding(bottom = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
             Spacer(modifier = Modifier.width(10.dp))
@@ -252,7 +280,6 @@ fun PhoneNumberVerifyCompose(countryList: List<String>) {
                 textAlign = TextAlign.Center
             )
         )
-
         Box(
             modifier = Modifier
                 .padding(horizontal = 40.dp)
@@ -274,14 +301,14 @@ fun PhoneNumberVerifyCompose(countryList: List<String>) {
                         )
                     }
                     .clickable {
-                        mExpanded = !mExpanded
+                        countrySelection = !countrySelection
                     },
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Spacer(modifier = Modifier.width(10.dp))
                 Text(
                     modifier = Modifier.padding(vertical = 5.dp),
-                    text = mSelectedText, style = TextStyle(
+                    text = selectedCountry, style = TextStyle(
                         fontSize = 16.sp,
                         color = lightSilver
                     )
@@ -297,8 +324,8 @@ fun PhoneNumberVerifyCompose(countryList: List<String>) {
                 modifier = Modifier
                     .height(250.dp)
                     .width(250.dp),
-                expanded = mExpanded,
-                onDismissRequest = { mExpanded = false },
+                expanded = countrySelection,
+                onDismissRequest = { countrySelection = false },
             ) {
                 Box(modifier = Modifier.size(width = 250.dp, height = 250.dp)) {
                     LazyColumn {
@@ -306,41 +333,224 @@ fun PhoneNumberVerifyCompose(countryList: List<String>) {
                             DropdownMenuItem(text = {
                                 Text(text = item)
                             }, onClick = {
-                                mSelectedText = item
-                                mExpanded = false
+                                selectedCountry = item
+                                countrySelection = false
                             })
                         }
                     }
                 }
             }
+
         }
+        Spacer(modifier = Modifier.height(10.dp))
         Row(
             Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 40.dp, vertical = 10.dp)
         ) {
-            Row(modifier = Modifier
-                .width(80.dp)
-                .clickable {
-                }, horizontalArrangement = Arrangement.SpaceBetween) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = "Country Code",
-                    tint = secondaryTextColor
-                )
-                Text(text = "1")
-                Spacer(modifier = Modifier.width(10.dp))
-            }
-            Spacer(modifier = Modifier.width(10.dp))
-            TextField(
+            Row(
                 modifier = Modifier
-                    .weight(1f)
-                    .background(color = Color.Black),
-                value = countryCode.toString(),
-                onValueChange = {
-                    countryCode = it.toInt()
-                })
+                    .width(80.dp)
+                    .drawBehind {
+                        val strokeWidth = 2f
+                        val y = size.height - strokeWidth / 2
+                        drawLine(
+                            primaryColor,
+                            Offset(0f, y),
+                            Offset(size.width, y),
+                            strokeWidth
+                        )
+                    }
+            ) {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .clickable {
 
+                        }) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { countryCodeSelection = !countryCodeSelection },
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(15.dp),
+                            imageVector = Icons.Default.Add,
+                            contentDescription = "Country Code",
+                            tint = secondaryTextColor
+                        )
+                        Text(
+                            text = selectedCountryCode.toString(),
+                            style = TextStyle(color = lightSilver, fontSize = 15.sp)
+                        )
+                        Spacer(modifier = Modifier.width(1.dp))
+                    }
+                    Spacer(modifier = Modifier.height(5.dp))
+                    DropdownMenu(
+                        modifier = Modifier
+                            .height(250.dp)
+                            .width(80.dp),
+                        expanded = countryCodeSelection,
+                        onDismissRequest = { countryCodeSelection = false },
+                    ) {
+                        Box(modifier = Modifier.size(width = 250.dp, height = 250.dp)) {
+                            LazyColumn {
+                                items(countryCodeList) { item ->
+                                    DropdownMenuItem(text = {
+                                        Text(text = item.toString())
+                                    }, onClick = {
+                                        selectedCountryCode = item
+                                        countryCodeSelection = false
+                                    })
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.width(15.dp))
+            BasicTextField(
+                modifier = Modifier
+                    .background(color = Color.Transparent)
+                    .weight(1f)
+                    .drawBehind {
+                        val strokeWidth = 2f
+                        val y = size.height - strokeWidth / 2
+                        drawLine(
+                            primaryColor,
+                            Offset(0f, y),
+                            Offset(size.width, y),
+                            strokeWidth
+                        )
+                    }
+                    .padding(bottom = 5.dp),
+                value = phoneNumber,
+                onValueChange = {
+                    phoneNumber = it
+                },
+                textStyle = TextStyle(color = lightSilver, fontSize = 15.sp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                cursorBrush = SolidColor(lightSilver)
+            )
+
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(text = "Carrier SMS charges may apply", style = TextStyle(color = secondaryTextColor))
+        Spacer(modifier = Modifier.height(10.dp))
+        if (isOTPSent) {
+            BasicTextField(
+                modifier = Modifier
+                    .background(color = Color.Transparent)
+                    .drawBehind {
+                        val strokeWidth = 2f
+                        val y = size.height - strokeWidth / 2
+                        drawLine(
+                            primaryColor,
+                            Offset(0f, y),
+                            Offset(size.width, y),
+                            strokeWidth
+                        )
+                    }
+                    .padding(bottom = 5.dp),
+                value = OTP,
+                onValueChange = {
+                    OTP = it
+                },
+                textStyle = TextStyle(
+                    color = lightSilver,
+                    fontSize = 15.sp,
+                    textAlign = TextAlign.Center
+                ),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                cursorBrush = SolidColor(lightSilver)
+            )
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = primaryColor,
+                contentColor = Color.Black
+            ), onClick = {
+                if (isOTPSent)
+                    onVerificationComplete()
+                isOTPSent = true
+            }, shape = RectangleShape
+        ) {
+            Text(text = if (isOTPSent) "CONTINUE" else "NEXT")
+        }
+    }
+}
+
+@Composable
+fun UpdateProfileInfoCompose(
+    onUpdateComplete: () -> Unit
+) {
+
+    var userName by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(whatsAppBackground)
+            .padding(horizontal = 20.dp, vertical = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Profile Information",
+            style = TextStyle(color = Color.White, fontSize = 18.sp)
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = "Please provide your Name and Profile Picture (Optional)",
+            style = TextStyle(color = secondaryTextColor)
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        Image(
+            modifier = Modifier
+                .size(96.dp)
+                .clip(CircleShape)
+                .border(4.dp, primaryColor, CircleShape),
+            painter = painterResource(id = R.drawable.default_user_icon),
+            contentDescription = "Default User Profile Pic",
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        BasicTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 20.dp, horizontal = 30.dp)
+                .drawBehind {
+                    val strokeWidth = 2f
+                    val y = size.height - strokeWidth / 2
+
+                    drawLine(
+                        primaryColor,
+                        Offset(0f, y),
+                        Offset(size.width, y),
+                        strokeWidth
+                    )
+                }
+                .padding(bottom = 5.dp),
+            value = userName,
+            onValueChange = {
+                userName = it
+            },
+            textStyle = TextStyle(
+                fontSize = 16.sp,
+                color = Color.White
+            ),
+            cursorBrush = SolidColor(primaryColor),
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            onClick = onUpdateComplete,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = primaryColor,
+                contentColor = Color.Black
+            ),
+            shape = RectangleShape
+        ) {
+            Text(text = "NEXT")
         }
     }
 }
